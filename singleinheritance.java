5a218949bc8178dcc883dbee1309d797e74b76d0
Dev <codekidinprogress@gmail.com>
2026-01-14 19:30:45 +0530

    Add Practical No. 04 of Single Inheritance
    Aim is to write a java program to implement single level inheritance.

---
+++ singleinheritance.java
@@ -0,0 +1,27 @@
+package Practical_04;
+
+//Parent class
+class Vehicle {
+	void start() {
+		System.out.println("Vehicle is starting...");
+	}
+}
+
+//Child class
+class Car extends Vehicle {
+	void drive() {
+		System.out.println("Car is driving smoothly.");
+	}
+}
+
+public class singleinheritance {
+
+	public static void main(String[] args) {
+		
+		Car myCar = new Car();//Object of child class 	      
+		myCar.start();//Inherited from vehicle  		        
+		myCar.drive();//From car  
+	}
+
+}
+
